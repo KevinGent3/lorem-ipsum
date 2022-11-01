10a3e74e918fba730523aea877a4ed732b6fb435
Kévin Gentilini <kevingentilini@macbook-pro.home>
2022-11-01 21:44:36 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Content from "./components/Content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Content
+        title={"Lorem"}
+        text={
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate dolor consectetur officiis voluptas, reprehenderit illum aut eligendi laborum aperiam maiores et tempora explicabo, quibusdam earum totam pariatur dolores temporibus iste!"
+        }
+      />
+      <Content
+        title={"Ipsum"}
+        text={
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate dolor consectetur officiis voluptas, reprehenderit illum aut eligendi laborum aperiam maiores et tempora explicabo, quibusdam earum totam pariatur dolores temporibus iste!"
+        }
+      />
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,4 @@
+const Title = (props) => {
+  return <h2>{props.name}</h2>;
+};
+export default Title;
